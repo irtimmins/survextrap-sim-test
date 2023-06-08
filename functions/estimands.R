ff8a37b7233d1cb6a3f5e51ca5bc5f5c68a81d21
Iain Timmins <113117134+irtimmins@users.noreply.github.com>
2023-06-08 11:08:21 +0100

    Update estimands.R

--- functions/estimands.R
+++ functions/estimands.R
@@ -6,24 +6,31 @@
 
 
 estimands <- function(true_mod, fit_mod, maxT){
-
+ 
+  ###############################
+  # rmst estimands
+  ###############################
+  
+  true_rmst <- standsurv(true_mod$true_mod, t=maxT, type="rmst")[[2]]
+  fit_rmst <- rmst(fit_mod$fit_mod, t=maxT)[[3]]
+ 
+  ###############################
+  # area betweem hazard curves
+  ###############################
+  
   # increments for numerical integration
   # needs to be >= 10^4 for accuracy but slow.
   t.N <- 10^3
-  
   t.vec <- seq(from = 0, to = maxT, length.out = t.N)
 
   haz_df <- tibble("true" =  standsurv(true_mod$true_mod, t=t.vec,type="hazard")[[2]],
                    "fit" = hazard(fit_mod$fit_mod, t=t.vec)[[2]])
 
-
   area_diff <- sum(abs(haz_df$true-haz_df$fit))/(t.N/(maxT-0))
   
+  ###############################
   
-  true_rmst <- standsurv(true_mod$true_mod, t=maxT, type="rmst")[[2]]
-  fit_rmst <- rmst(fit_mod$fit_mod, t=maxT)[[3]]
-  
-  return(list(area_diff=area_diff, true_rmst=true_rmst, fit_rmst=fit_rmst)) 
+  return(list(true_rmst=true_rmst, fit_rmst=fit_rmst, area_diff=area_diff))
   
 }
 
